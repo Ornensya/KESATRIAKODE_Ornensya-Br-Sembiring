2f8c5ee0cce385ae1453b69e5275299560365770
Ornensya <ornensyasembiring@gmail.com>
2023-07-03 14:04:52 +0700

    Menambah variabel,script,inline

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Ini file yang terpisah")
+console.log("Ini file yang terpisah");
+
+var kalimat = "Ini Pelatihan";
+
+for(var i = 0; i <5; i++){
+    document.write(kalimat + "<br>");
+}
