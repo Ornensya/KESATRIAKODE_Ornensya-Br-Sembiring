71e218885556dd4bbd0fde62f6502d5fd1863561
Ornensya <ornensyasembiring@gmail.com>
2023-07-04 11:37:54 +0700

    Menambah program aritmatika

--- script2.js
+++ script2.js
@@ -1,4 +1,12 @@
+// Variabel const harus diisi nilai dan nilainya tidak akan berubah saat dipanggil atau dapat disebut nilai tetap
 const nama = "Ornensya";
-nama = "Ornen"
+nama = "Ornen";
 
+/* Tipe data let digunakan untuk variabel yang masih kosong dan juga 
+untuk menghindari data dapat diakses secara global */
+let fullName;
+let FullName;
 console.log(nama);
+
+// postTitle digunakan unutk membuat judul postingan
+let postTitle = "Tutorial javascript untuk pemula ";
